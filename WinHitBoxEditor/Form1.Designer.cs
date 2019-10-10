fdff1d0686b0ec5467e79485ec78b277d3c52ee4
Alex Shisler <kaiensai@gmail.com>
2019-10-10 12:13:42 -0400

    Initial commit

---
+++ WinHitBoxEditor/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WinHitBoxEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imagePane = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.moveButton = new System.Windows.Forms.Button();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.rectangleButton = new System.Windows.Forms.Button();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imagePane)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imagePane
+            // 
+            this.imagePane.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imagePane.Location = new System.Drawing.Point(15, 47);
+            this.imagePane.Name = "imagePane";
+            this.imagePane.Size = new System.Drawing.Size(773, 391);
+            this.imagePane.TabIndex = 0;
+            this.imagePane.TabStop = false;
+            this.imagePane.MouseDown += new System.Windows.Forms.MouseEventHandler(this.imagePane_MouseDown);
+            this.imagePane.MouseMove += new System.Windows.Forms.MouseEventHandler(this.imagePane_MouseMove);
+            this.imagePane.MouseUp += new System.Windows.Forms.MouseEventHandler(this.imagePane_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.moveButton);
+            this.panel1.Controls.Add(this.selectButton);
+            this.panel1.Controls.Add(this.rectangleButton);
+            this.panel1.Controls.Add(this.exportButton);
+            this.panel1.Controls.Add(this.openButton);
+            this.panel1.Location = new System.Drawing.Point(12, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 38);
+            this.panel1.TabIndex = 1;
+            // 
+            // moveButton
+            // 
+            this.moveButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.moveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moveButton.Location = new System.Drawing.Point(252, 3);
+            this.moveButton.Name = "moveButton";
+            this.moveButton.Size = new System.Drawing.Size(75, 32);
+            this.moveButton.TabIndex = 4;
+            this.moveButton.Text = "Move";
+            this.moveButton.UseVisualStyleBackColor = false;
+            this.moveButton.Click += new System.EventHandler(this.moveButton_Click);
+            // 
+            // selectButton
+            // 
+            this.selectButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.selectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectButton.Location = new System.Drawing.Point(162, 3);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(84, 32);
+            this.selectButton.TabIndex = 3;
+            this.selectButton.Text = "Select";
+            this.selectButton.UseVisualStyleBackColor = false;
+            this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
+            // 
+            // rectangleButton
+            // 
+            this.rectangleButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.rectangleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rectangleButton.Location = new System.Drawing.Point(333, 3);
+            this.rectangleButton.Name = "rectangleButton";
+            this.rectangleButton.Size = new System.Drawing.Size(89, 32);
+            this.rectangleButton.TabIndex = 2;
+            this.rectangleButton.Text = "Rectangle";
+            this.rectangleButton.UseVisualStyleBackColor = false;
+            this.rectangleButton.Click += new System.EventHandler(this.rectangleButton_Click);
+            // 
+            // exportButton
+            // 
+            this.exportButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.exportButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exportButton.Location = new System.Drawing.Point(83, 3);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(73, 32);
+            this.exportButton.TabIndex = 1;
+            this.exportButton.Text = "Export";
+            this.exportButton.UseVisualStyleBackColor = false;
+            // 
+            // openButton
+            // 
+            this.openButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.openButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openButton.Location = new System.Drawing.Point(3, 3);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(74, 32);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = false;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.imagePane);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imagePane)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imagePane;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button exportButton;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button rectangleButton;
+        private System.Windows.Forms.Button moveButton;
+        private System.Windows.Forms.Button selectButton;
+    }
+}
+
---
+++ WinHitBoxEditor/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Serilog;
+using Serilog.Core;
+
+namespace WinHitBoxEditor
+{
+    public partial class Form1 : Form
+    {
+        public Item CurrentItem;
+        public Color PaintColor;
+        public SolidBrush semiTransparentBrush;
+        public List<Rectangle> rectangles;
+
+        public bool CanImageFit { get; set; }
+        public bool IsDown { get; set; }
+        public bool IsSelected { get; set; }
+        public int SelectedRectangle { get; set; }
+        public int OriginX { get; set; }
+        public int OriginY { get; set; }
+        public int EndX { get; set; }
+        public int EndY { get; set; }
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            rectangles = new List<Rectangle>();
+            PaintColor = Color.FromArgb(128, 10, 10, 100);
+            semiTransparentBrush = new SolidBrush(PaintColor);
+            Log.Logger = new LoggerConfiguration().
+                MinimumLevel.Debug().
+                WriteTo.Console()
+                .WriteTo.File(@"F:\\C# Stuff\\HitBoxWinformRepo\\HitBoxEditor\\WinHitBoxEditor\\Logging\\log.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+
+        }
+
+        public enum Item
+        {
+            Resize, Move, Rectangle
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "All supported graphics | *.jpg; *.jpeg; *.png";
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                imagePane.Image = new Bitmap(openFile.FileName);
+                CanImageFit = (imagePane.Image.Width < imagePane.Width && imagePane.Image.Height < imagePane.Height);
+                if (CanImageFit)
+                    imagePane.SizeMode = PictureBoxSizeMode.CenterImage;
+                else
+                    imagePane.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+        }
+
+        private void selectButton_Click(object sender, EventArgs e)
+        {
+            CurrentItem = Item.Resize;
+        }
+
+        private void moveButton_Click(object sender, EventArgs e)
+        {
+            CurrentItem = Item.Move;
+        }
+
+        private void rectangleButton_Click(object sender, EventArgs e)
+        {
+            CurrentItem = Item.Rectangle;
+        }
+
+        private void imagePane_MouseDown(object sender, MouseEventArgs e)
+        {
+            IsDown = true;
+            OriginX = e.X;
+            OriginY = e.Y;
+            var count = 0;
+            for (int i = 0; i < rectangles.Count; i++)
+            {
+                if (rectangles[i].Contains(e.Location))
+                {
+
+                    SelectedRectangle = i;
+                    Log.Debug("Selected Rectangle Index: " + SelectedRectangle);
+
+                    IsSelected = true;
+                    count++;
+
+
+                }
+
+            }
+            if (count == 0)
+            {
+                Log.Debug("Not selected");
+                IsSelected = false;
+            }
+
+            if (CurrentItem == Item.Resize)
+            {
+
+            }
+
+        }
+
+        private void imagePane_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (IsDown)
+            {
+                Graphics graphics = imagePane.CreateGraphics();
+                switch (CurrentItem)
+                {
+                    case Item.Rectangle:
+                        this.Refresh();
+                        foreach (var rectangle in rectangles)
+                        {
+                            graphics.FillRectangle(semiTransparentBrush, rectangle);
+                        }
+                        var rect = new Rectangle(OriginX, OriginY, e.X - OriginX, e.Y - OriginY);
+                        graphics.FillRectangle(semiTransparentBrush, rect);
+
+                        break;
+                    case Item.Move:
+                        var newRect = rectangles[SelectedRectangle];
+                        newRect.X = e.X;
+                        newRect.Y = e.Y;
+                        rectangles[SelectedRectangle] = newRect;
+                        this.Refresh();//RefreshCanvas(graphics);
+                        Log.Debug("Selected Rectangle Index: " + SelectedRectangle);
+                        foreach (var rectangle in rectangles)
+                        {
+                            graphics.FillRectangle(semiTransparentBrush, rectangle);
+                        }
+                        break;
+
+                }
+
+                graphics.Dispose();
+            }
+
+        }
+
+        private void imagePane_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (IsDown)
+            {
+                EndX = e.X;
+                EndY = e.Y;
+                IsDown = false;
+
+                Graphics graphics = imagePane.CreateGraphics();
+
+                switch (CurrentItem)
+                {
+                    case Item.Rectangle:
+                        rectangles.Add(new Rectangle(OriginX, OriginY, EndX - OriginX, EndY - OriginY));
+                        this.Refresh();//RefreshCanvas(graphics);
+                        foreach (var rectangle in rectangles)
+                        {
+                            graphics.FillRectangle(semiTransparentBrush, rectangle);
+                        }
+                        break;
+                }
+
+                graphics.Dispose();
+            }
+        }
+
+        private void RefreshCanvas(Graphics graphics)
+        {
+            graphics.Clear(imagePane.BackColor);
+            if (imagePane.Image != null)
+            {
+                if (!CanImageFit)
+                    graphics.DrawImage(imagePane.Image, new Rectangle(0, 0, imagePane.Width, imagePane.Height));
+                else
+                    graphics.DrawImage(imagePane.Image, new Rectangle(imagePane.Width / 2, imagePane.Height / 2, imagePane.Width, imagePane.Height));
+            }
+            Console.WriteLine(rectangles.Count);
+            foreach (var rectangle in rectangles)
+            {
+                graphics.FillRectangle(semiTransparentBrush, rectangle);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Log.CloseAndFlush();
+        }
+    }
+}
